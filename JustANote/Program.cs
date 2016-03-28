fd7efb69424e183f185befad60e0929c05393062
JumboShwen <shwen@tom.com>
2016-03-28 10:06:00 +0800

    small changes

--- JustANote/Program.cs
+++ JustANote/Program.cs
@@ -18,36 +18,5 @@ namespace JustANote
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-
-        static readonly Guid CLSID_ActiveDesktop = new Guid("{75048700-EF1F-11D0-9888-006097DEACF9}");
-
-        public static IActiveDesktop GetActiveDesktop()
-        {
-            Type typeActiveDesktop = Type.GetTypeFromCLSID(CLSID_ActiveDesktop);
-            return (IActiveDesktop)Activator.CreateInstance(typeActiveDesktop);
-        }
-
-        public static void Test()
-        {
-            var sb = new StringBuilder(1024);
-
-            var ad = GetActiveDesktop();
-            var ret = ad.GetWallpaper(sb, 1024, Constants.AD_GETWP_LAST_APPLIED);
-
-            Console.WriteLine(ret);
-            Console.WriteLine(sb);
-
-            //ret = ad.SetWallpaper(@"C:\Windows\Web\Wallpaper\Theme1\img1.jpg", 0);
-            ret = ad.SetWallpaper(@"C:\Windows\web\wallpaper\Windows\img0.jpg", 0);
-            Console.WriteLine(ret);
-
-            ret = ad.ApplyChanges(AD_Apply.ALL);
-            Console.WriteLine(ret);
-
-            ret = ad.GetWallpaper(sb, 1024, Constants.AD_GETWP_LAST_APPLIED);
-            Console.WriteLine(ret);
-            Console.WriteLine(sb);
-
-        }
     }
 }
